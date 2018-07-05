fd76b855e7a1ac8e0183737320e1ee21cb2a19a4
Dheeraj Krishna Kunam <31747946+kdkrishna2187@users.noreply.github.com>
2018-07-05 11:50:00 -0500

    urlify solution added

--- Arrays and Strings/URLify.java
+++ Arrays and Strings/URLify.java
@@ -1,3 +1,35 @@
 public class URLify{
-    
+   /*
+   Two pass approach. Find the newLength and modify the character array
+   TC -> O(n) 
+   */
+   
+    public String URLify1(String s , int trueLength){
+        // counting the number of spaces
+        int len = s.length();
+        char [] str = s.toCharArray();
+        int spaces = 0;
+        for(int i = 0; i < trueLength ; ++i ){
+            if(str[i]=' '){
+                spaces++;
+            }
+        } 
+        int newLength = trueLength + 2 * spaces;
+        if(trueLength < str.length){
+            str[trueLength] = '\0'; // ending the array due to excess spaces
+        }
+        for(int j = trueLength - 1; j >=0 ; j--){
+            if(str[j] = ' '){
+                str[newLength - 1] = '0';
+                str[newLength - 2] = '2';
+                str[newLength - 3] = '%';
+                newLength = newLength -3 ;
+            }
+            else{
+                str[newLength - 1] = str[j];
+                newLength--;
+            }
+        }
+        return new String(str);
+    }
 }
