a1999e9c895b0e7ed77ea02bac0959e80b647b21
Dheeraj Krishna Kunam <31747946+kdkrishna2187@users.noreply.github.com>
2018-07-13 12:57:49 -0500

    single pass solution added

--- Arrays and Strings/PalindromePermutation.java
+++ Arrays and Strings/PalindromePermutation.java
@@ -1,7 +1,8 @@
 import java.util.HashSet;
 
 public class PalindromePermutation{
-    /* Using hashSet. We add the character if it's present in the set else remove it
+    /*
+	/* Using hashSet. We add the character if it's present in the set else remove it
     if the size is less than or equal to 1 return true.
     TC:O(n) SC:O(n)
     */
@@ -17,12 +18,24 @@ public class PalindromePermutation{
                 hset.add(letter);
             }
         }
-        
-        if(hset.size()<=1){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return hset.size() <= 1;
     }
+    /* Single pass solution: Taking array map with length 128 ASCII characters
+    increment the count for odd occurences and decrement for even occurence
+    TC:O(n) SC: O(1)  */
+    public static boolean PalindromePermutation2(String phrase){
+        int [] map = new int[128];
+        int phrase_length = phrase.length();
+        int count = 0;
+        for(int index = 0; index < phrase_length;++index){
+            map[phrase.charAt(index)]++;
+            if(map[phrase.charAt(index)]%2){
+                count --;
+            }
+            else{
+                count++;
+            }
+        }
+        return count <= 1;
+    }    
 }
